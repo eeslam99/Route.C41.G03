7242e66f45f74a5c4f846abc085a5e612fd5e73c
Gaming Store <Gaming Store@DESKTOP-6FL3FUL>
2024-04-04 22:29:35 +0200

    Department Update & Delete

--- Route.C41.G03.PL/Controllers/DepartmentController.cs
+++ Route.C41.G03.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Route.C41.G03.BLL.Interfaces;
+using Route.C41.G03.DAL.Models;
 
 namespace Route.C41.G03.PL.Controllers
 {
@@ -20,17 +21,100 @@ namespace Route.C41.G03.PL.Controllers
         }
 
         [HttpGet]
-        public IActionResult Details(int?id)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _departmentRepo.Add(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (System.Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id , string viewName = "Edit")
         {
             if (!id.HasValue)
                 return BadRequest();
             var department = _departmentRepo.Get(id.Value);
-            
             if (department == null)
                 return NotFound();
-
             return View(department);
+        }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (!id.HasValue)
+            //    return BadRequest();
+            //var department =_departmentRepo.Get(id.Value);
+            //if(department is null)
+            //    return NotFound();
+            //return View(department);
+
+            return Details(id , "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Department department ,[FromRoute] int id)
+        {   
+            if(id!=department.Id)
+                return BadRequest();
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _departmentRepo.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch(System.Exception ex)
+                {
+                    //1. log Exception
+                    //2.form
+
+                    ModelState.AddModelError(string.Empty, ex.Message); 
+
+                }
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            return Details(id, "Delete");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Department department, [FromRoute] int id )
+        {
+            if(id!=department.Id)
+                return BadRequest();
+            try
+            {
+                _departmentRepo.Delete(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch( System.Exception ex)
+            {
+                ModelState.AddModelError (string.Empty, ex.Message);    
+            }
+            return View(department);
         }
     }
 }
